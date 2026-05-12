6afafa6de5ec9b0c9a91061eab1f99939bf1f513
Noah Elias Kastner <elias@kastner-home.com>
2026-05-12 08:34:19 +0200

    added coding languages and managers

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# update installed packages
+sudo pacman -Syu --noconfirm
+
 # install paru
 sudo pacman -S -needed git base-devel
 git clone https://aur.archlinux.org/paru.git
@@ -8,11 +11,8 @@ makepkg -si
 cd ..
 rm -rf paru
 
-# update all installed packages
-paru -Syu
-
 # install additional packages
-PACKAGES=(
+PACKAGES_TO_INSTALL=(
     # hyprland
     "hyprland"
     "hyprlock"
@@ -21,17 +21,24 @@ PACKAGES=(
     # base
     "librewolf-bin"
     "thunderbird"
-    # programming
+    # ides
     "neovim"
     "code"
     "rider"
     "unityhub"
+    # coding languages and managers
+    "rustup"
+    "nodejs"
+    "npm"
 )
-paru -S --needed --noconfirm "${PACKAGES[@]}"
+paru -S --needed --noconfirm "${PACKAGES_TO_INSTALL[@]}"
 
 # enable zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /usr/bin/zsh
+
+# set rust default
+rustup default stable
 
 # hints
 cat << EOF
