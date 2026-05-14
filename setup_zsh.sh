2ce54c751f4b6271a1ea1caceb88e0d6a176704e
Noah Elias Kastner <elias@kastner-home.com>
2026-05-14 08:28:53 +0200

    fixed setup_zsh.sh (syntax error)

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions
-sh -c "curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /usr/bin/zsh
 
