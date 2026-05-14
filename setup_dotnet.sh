c09ce23a487da555393d7544ea1e296ff086f189
Noah Elias Kastner <elias@kastner-home.com>
2026-05-14 09:18:37 +0200

    hopefully fixed setup_dotnet.sh

--- setup_dotnet.sh
+++ setup_dotnet.sh
@@ -5,15 +5,15 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 "$SCRIPT_DIR/setup_paru.sh"
 
 PACKAGES=(
-    # ide
-    "rider"
-    # runtimes
-    "dotnet-runtime"
-    "asp-runtime"
     # sdk stuff
     "dotnet-sdk"
     "dotnet-host"
     "dotnet-targeting-pack"
+    # runtimes
+    "dotnet-runtime"
+    "aspnet-runtime"
+    # ides
+    "rider"
 )
 
 paru -S --needed --noconfirm "${PACKAGES[@]}"
