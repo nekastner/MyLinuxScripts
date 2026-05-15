d045fa0d89ad0a4aea5a56ade2b1ffdf7925c57d
Noah Elias Kastner <elias@kastner-home.com>
2026-05-15 13:04:39 +0200

    made setup dev scripts independant from paru and removed ides

--- setup_dotnet_dev.sh
+++ setup_dotnet_dev.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-"$SCRIPT_DIR/setup_paru.sh"
-
-PACKAGES=(
-    # sdk stuff
-    "dotnet-sdk"
-    "dotnet-host"
-    "dotnet-targeting-pack"
-    # runtimes
-    "dotnet-runtime"
-    "aspnet-runtime"
-    # ides
-    "rider"
-)
-
-paru -S --needed --noconfirm "${PACKAGES[@]}"
+sudo pacman -S --needed --noconfirm dotnet-sdk dotnet-host dotnet-targeting-pack dotnet-runtime aspnet-runtime
 
--- setup_web_dev.sh
+++ setup_web_dev.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-"$SCRIPT_DIR/setup_paru.sh"
-
-PACKAGES=(
-    # base
-    "nodejs"
-    "npm"
-    # ides
-    "webstorm"
-)
-
-paru -S --needed --noconfirm "${PACKAGES[@]}"
+sudo pacman -S --needed --noconfirm nodejs npm
 
