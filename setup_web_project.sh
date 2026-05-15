3e223776c4510a36c66e8b2249115992e178ebb5
Noah Elias Kastner <elias@kastner-home.com>
2026-05-15 15:01:11 +0200

    added script for creating empty web projects with react ts and tailwindcss

---
+++ setup_web_project.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# check if the project name is given
+if [ -z "$1" ]; then
+    echo "!!! ERR: Project name required."
+    exit 1
+fi
+
+# create vite project
+npm create vite@latest "$1" --yes -- --template react-ts &
+wait $!
+
+# go into project directory
+cd "$1" || { echo "!!! ERR: Unable to find project directory '$1'"; exit 1; }
+
+# install tailwind with dependencies
+npm install -D tailwindcss @tailwindcss/postcss postcss autoprefixer
+
+# setup postcss.config.js
+cat << "EOF" > postcss.config.js
+export default {
+    plugins: {
+	"@tailwindcss/postcss": {},
+	autoprefixer: {},
+    },
+}
+EOF
+
+# setup src/tailwind.css
+cat << "EOF" > src/tailwind.css
+@import "tailwindcss";
+EOF
+
+# remove src/index.css
+rm src/index.css
+
+# setup src/main.tsx
+sed -i "1i import \"./tailwind.css\"" src/main.tsx
+sed -i "/index\.css/d" src/main.tsx
+sed -i "s/'\''/\"/g" src/main.tsx
+
+# setup src/App.tsx
+cat << "EOF" > src/App.tsx
+import "./tailwind.css"
+import react from "react";
+
+function App() {
+    return (
+	<div className="h-screen bg-red-500 flex items-center justify-center">
+	    <h1 className="text-white text-5xl font-black uppercase">
+		Hello World!
+	    </h1>
+	</div>
+    )
+}
+
+export default App
+EOF
+
+# remove src/App.css
+rm src/App.css
+
+# execute
+npm run dev
+
