a79b2264f3d5b59b750c16b768a19e0a2f1cc46a
Noah Elias Kastner <elias@kastner-home.com>
2026-05-19 09:16:46 +0200

    modified install_cisco_packettracer.sh

--- install_cisco_packettracer.sh
+++ install_cisco_packettracer.sh
@@ -2,6 +2,7 @@
 
 GIT_REPO_URL="https://aur.archlinux.org/packettracer.git"
 TEMP_PATH="$HOME/.install_cisco_packettracer_temp"
+INSTALL_PATH="/usr/lib/packettracer"
 
 mkdir -p "$TEMP_PATH"
 
@@ -10,16 +11,14 @@ git clone "$GIT_REPO_URL" "$TEMP_PATH"
 read -p "Download .deb package from 'https://www.netacad.com/resources/lab-downloads' into $TEMP_PATH. Press Enter to continue..." --
 
 # build
-makepkg -si
+makepkg -si -D "$TEMP_PATH"
 
 # remove trash
 rm rf "$TEMP_PATH"
 
 # create executable app
-cd /usr/lib/packettracer/
-sudo ./packettracer.AppImage --appimage-extract
-squashfs-root/AppRun &
-cd "$HOME"
+sudo "$INSTALL_PATH/packettracer.AppImage" --appimage-extract
+"$INSTALL_PATH/squashfs-root/AppRun" &
 
 # copy icon
 sudo cp /usr/lib/packettracer/squashfs-root/app.png /usr/share/icons/hicolor/48x48/apps/cisco-packettracer.png
@@ -31,7 +30,4 @@ Icon=cisco-packettracer
 Type=Application
 Categories=Education;
 Keywords=cisco;packettracer;" | sudo tee /usr/share/applications/cisco-packettracer.desktop > /dev/null
-
-# hints
-echo "Note: For the in-app-login, you need to use the in-app-web-browser. This can be selected in the settings of Cisco PacketTracer."
 
